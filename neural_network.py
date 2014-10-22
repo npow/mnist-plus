8accc5256348276b9477b92cc710bcc808de8690
npow <nissan.pow@gmail.com>
2014-10-21 23:30:22 -0400

    bug fixes

--- neural_network.py
+++ neural_network.py
@@ -4,6 +4,7 @@ from __future__ import division
 import math
 import numpy as np
 import random
+import sys
 from nolearn.dbn import DBN
 from sklearn.cross_validation import cross_val_score, train_test_split
 from sklearn.datasets import load_iris
@@ -13,10 +14,7 @@ from sklearn import metrics
 np.random.seed(2)
 
 def g(x):
-  try:
-    return 1 / (1 + math.exp(-x))
-  except:
-    return 0
+  return 1 / (1 + math.exp(-x))
 
 def g1(x):
   return (1 - g(x))*g(x)
@@ -35,17 +33,15 @@ class NeuralNetwork:
     for j in xrange(0, len(layer_sizes)-1):
       b = math.sqrt(6) / math.sqrt(layer_sizes[j] + layer_sizes[j+1])
       self.W[j] = np.random.uniform(-b, b, [layer_sizes[j+1], layer_sizes[j]+1])
-    #self.bias = np.random.uniform(-1/layer_sizes[0], 1/layer_sizes[0], [len(layer_sizes)-1, 1])
-    self.bias = [0] * len(layer_sizes)
+    self.bias = [1] * len(layer_sizes)
 
   def forward_propagate(self, X):
     self.A[0] = X
     for j in xrange(1, len(self.layer_sizes)):
       A = self.W[j-1]
-      #B = np.append([self.bias[j-1]], self.A[j-1])
-      B = np.append([self.bias[j-1]], X) # FIXME: Is this supposed to be X or A[j-1]?
+      B = np.append([self.bias[j-1]], self.A[j-1])
       B = B.reshape((B.shape[0], 1))
-      Z = np.dot(A, B) # FIXME: Is this correct?
+      Z = np.dot(A, B)
       for i, z in enumerate(Z):
         self.A[j][i] = g(z)
 
@@ -53,11 +49,9 @@ class NeuralNetwork:
     delta = [np.zeros((self.layer_sizes[l], 1)) for l in xrange(len(self.layer_sizes))]
     for l in xrange(len(self.layer_sizes)-1, 0, -1):
       if l == len(self.layer_sizes) - 1:
-        #delta[l] = self.A[l] - Y
-        delta[l] = 0.5 * (self.A[l] - Y)**2 # FIXME: Is this correct?
+        delta[l] = self.A[l] - Y
       else:
-        #D = np.append([self.bias[l-1]], self.A[l-1])
-        D = np.append([self.bias[l-1]], X) # FIXME: Is this supposed to be X or A[l-1]?
+        D = np.append([self.bias[l-1]], self.A[l-1])
         D = D.reshape((D.shape[0], 1))
         Z = np.dot(self.W[l-1], D)
         A = np.dot(self.W[l].T, delta[l+1])
@@ -68,14 +62,14 @@ class NeuralNetwork:
       A = delta[l+1]
       B = np.append([self.bias[l]], self.A[l])
       B = B.reshape((B.shape[0], 1))
-      self.W[l] += self.alpha * np.dot(A, B.T) / self.Nx # FIXME: Also update bias?
+      self.W[l] -= self.alpha * np.dot(A, B.T)
 
   def fit(self, X, Y):
     self.Nc = np.unique(Y).shape[0]
     self.Nx = X.shape[0]
     indices = range(X.shape[0])
     for iter in xrange(self.epochs):
-      random.shuffle(indices)
+      #random.shuffle(indices)
       for i, idx in enumerate(indices):
         x = X[idx]
         y = np.zeros((self.Nc, 1))
@@ -93,14 +87,24 @@ class NeuralNetwork:
 
 iris = load_iris()
 
-X_train, X_test, Y_train, Y_test = train_test_split(scale(iris.data), iris.target, test_size=0.2, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(scale(iris.data), iris.target, test_size=0.5, random_state=42)
 #clf = DBN( [4, 4, 3], learn_rates=1, epochs=1, momentum=0, learn_rate_decays=1, minibatch_size=X_train.shape[0])
-clf = NeuralNetwork([4,4,3], alpha=1, epochs=1)
-clf.fit(X_train, Y_train)
-print "*" * 80
-pred = clf.predict(X_test)
+scores = []
+for epochs in xrange(100):
+  print epochs
+  clf = NeuralNetwork([4,4,3], alpha=0.1, epochs=epochs)
+  clf.fit(X_train, Y_train)
+  pred = clf.predict(X_test)
+  score = metrics.f1_score(Y_test, pred)
+  scores.append(score)
 
-score = metrics.f1_score(Y_test, pred)
+import matplotlib.pyplot as plt
+plt.plot(scores)
+plt.xlabel('epochs')
+plt.ylabel('f1 score')
+plt.savefig('graph.png')
+
+"""
 print("f1-score:   %0.3f" % score)
 
 print("classification report:")
@@ -108,3 +112,4 @@ print(metrics.classification_report(Y_test, pred))
 
 print("confusion matrix:")
 print(metrics.confusion_matrix(Y_test, pred))
+"""
