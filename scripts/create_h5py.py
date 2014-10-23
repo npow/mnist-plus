5f29735f860590b4f6994da6e777ae7a0ffd9176
Nissan Pow <nissan.pow@gmail.com>
2014-10-23 02:04:44 +0000

    fix filename

--- scripts/create_h5py.py
+++ scripts/create_h5py.py
@@ -2,8 +2,8 @@ import h5py
 import numpy as np
 from sklearn.cross_validation import train_test_split
 
-X = np.load('../blobs/X_train.npy')
-Y = np.load('../blobs/Y_train.npy')
+X = np.load('../blobs/X_train.npy')[:1000]
+Y = np.load('../blobs/Y_train.npy')[:1000]
 #X_test = np.load('X_test.npy')
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
@@ -12,12 +12,12 @@ with h5py.File('../blobs/train.h5', 'w') as f:
   f['data'] = X_train
   f['label'] = Y_train.astype(np.float32)
 with open('../blobs/train.txt', 'w') as f:
-  f.write('examples/h5classification/data/train.h5\n')
-  f.write('examples/h5classification/data/train.h5\n')
+  f.write('examples/hdf5_classification/data/train.h5\n')
+  f.write('examples/hdf5_classification/data/train.h5\n')
 
 with h5py.File('../blobs/test.h5', 'w') as f:
   f['data'] = X_test
   f['label'] = Y_test.astype(np.float32)
 with open('../blobs/test.txt', 'w') as f:
-  f.write('examples/h5classification/data/test.h5\n')
-  f.write('examples/h5classification/data/test.h5\n')
+  f.write('examples/hdf5_classification/data/test.h5\n')
+  f.write('examples/hdf5_classification/data/test.h5\n')
