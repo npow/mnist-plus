cd31ebd4c025877709e615d5fda60c62c6d75960
npow <nissan.pow@gmail.com>
2014-10-24 19:32:40 -0400

    update predict script

--- predict.py
+++ predict.py
@@ -1,11 +1,12 @@
-import numpy as np
 import caffe
+import numpy as np
+import os
 
-HOME = '/Users/npow'
+HOME = os.environ['HOME']
 MNIST_ROOT = '%s/code/mnist' % HOME
 CAFFE_ROOT = '%s/code/caffe' % HOME
 MODEL_FILE = '%s/examples/mnist/lenet.prototxt' % CAFFE_ROOT
-PRETRAINED = '%s/examples/mnist/npow_iter_1000.caffemodel' % CAFFE_ROOT
+PRETRAINED = '%s/examples/mnist/npow_iter_10000.caffemodel' % CAFFE_ROOT
 IMG_PATH = '%s/data/train_images' % MNIST_ROOT
 
 net = caffe.Classifier(MODEL_FILE, PRETRAINED, image_dims=(28,28))
@@ -16,5 +17,8 @@ def classify(id):
   prediction = net.predict([input_image])
   return prediction[0].argmax()
 
-for id in xrange(1, 10):
-  print classify(id)
+f = open('preds.csv', 'wb')
+for id in xrange(1, 20001):
+  klass = classify(id)
+  f.write('%d,%d\n' % (id, klass))
+f.close()
