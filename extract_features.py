00da07bb5d06764c5a71e12e9dad3f912c1e99c7
npow <nissan.pow@gmail.com>
2014-10-25 09:16:06 -0400

    .

---
+++ extract_features.py
@@ -0,0 +1,27 @@
+import caffe
+import numpy as np
+import os
+
+HOME = os.environ['HOME']
+MNIST_ROOT = '%s/code/mnist' % HOME
+CAFFE_ROOT = '%s/code/caffe' % HOME
+MODEL_FILE = '%s/examples/mnist/lenet.prototxt' % CAFFE_ROOT
+PRETRAINED = '%s/examples/mnist/npow_iter_10000.caffemodel' % CAFFE_ROOT
+IMG_PATH = '%s/data/train_images' % MNIST_ROOT
+
+net = caffe.Classifier(MODEL_FILE, PRETRAINED, image_dims=(28,28))
+net.set_phase_test()
+
+def classify(id):
+  input_image = caffe.io.load_image('%s/%d.png' % (IMG_PATH, id), color=False)
+  print input_image[1:10]
+  sys.ext
+  prediction = net.predict([input_image])
+  return prediction[0].argmax()
+
+f = open('preds.csv', 'wb')
+f.write('Id,Prediction\n')
+for id in xrange(1, 20001):
+  klass = classify(id)
+  f.write('%d,%d\n' % (id, klass))
+f.close()
