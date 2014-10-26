56286d9337a645cc9c6ceb3c62f287720e6921eb
npow <nissan.pow@gmail.com>
2014-10-26 09:50:44 -0400

    update predict script

--- predict.py
+++ predict.py
@@ -1,30 +1,42 @@
 import caffe
 import numpy as np
 import os
-from caffe.proto import caffe_pb2
 
 HOME = os.environ['HOME']
 MNIST_ROOT = '%s/code/mnist' % HOME
 CAFFE_ROOT = '%s/code/caffe' % HOME
-MODEL_FILE = '%s/examples/mnist/raw_lenet.prototxt' % CAFFE_ROOT
-PRETRAINED = '%s/examples/mnist/raw_submean_npow_iter_130000.caffemodel' % CAFFE_ROOT
-MEAN_FILE = '%s/raw_mean.binaryproto' % CAFFE_ROOT
+MODEL_FILE = '%s/examples/mnist/npow.prototxt' % CAFFE_ROOT
+PRETRAINED = '%s/examples/mnist/raw_npow_iter_85000.caffemodel' % CAFFE_ROOT
 IMG_PATH = '%s/data/test_images/' % MNIST_ROOT
 
-blob = caffe_pb2.BlobProto()
-data = open(MEAN_FILE, 'rb').read()
-blob.ParseFromString(data)
-IMG_MEAN = caffe.io.blobproto_to_array(blob).reshape(1, 48, 48)
-
-net = caffe.Classifier(MODEL_FILE, PRETRAINED, mean=IMG_MEAN, image_dims=(48,48))
+net = caffe.Classifier(MODEL_FILE, PRETRAINED, image_dims=(48,48))
 net.set_phase_test()
 
-images = [caffe.io.load_image('%s/%d.png' % (IMG_PATH, id), color=False) for id in xrange(1, 20001)]
-preds = map(lambda x: x[0].argmax(), net.predict(images))
+def classify(id):
+  image = caffe.io.load_image('%s/%d.png' % (IMG_PATH, id), color=False)
+  p = net.predict([image])
+  return p[0].argmax()
 
-f = open('preds_123000.csv', 'wb')
-f.write('Id,Prediction\n')
-for i, klass in enumerate(preds):
-  id = i+1
-  f.write('%d,%d\n' % (id, klass))
-f.close()
+def run_test():
+  IMG_PATH = '%s/data/train_images/' % MNIST_ROOT
+  Y = np.load('blobs/Y_train.npy')
+  c = 0
+  for id in xrange(1, 50001):
+    if id % 100 == 0:
+      print id
+    klass = classify(id)
+    if Y[id-1] != klass:
+      c += 1
+  print "error: %f" % (c / 50000)
+
+def create_submission():
+  f = open('preds_85000.csv', 'wb')
+  f.write('Id,Prediction\n')
+  for id in xrange(1, 20001):
+    if id % 100 == 0:
+      print id
+    klass = classify(id)
+    f.write('%d,%d\n' % (id, klass))
+  f.close()
+
+create_submission()
